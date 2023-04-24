44754aafb5af6f80ca828b71b65a2db0e290c7f6
José Antonio Galván Ramos <jose.galvan@softtek.com+1-at-467146794691>
2023-04-24 12:19:33 -0600

    update remote driver

--- MvnSeleniumExamples/src/main/java/com/fmk/base/TSD_BaseClassDrivers.java
+++ MvnSeleniumExamples/src/main/java/com/fmk/base/TSD_BaseClassDrivers.java
@@ -2,6 +2,7 @@ package com.fmk.base;
 
 import static org.testng.Assert.fail;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -11,12 +12,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import org.apache.log4j.Logger;
@@ -52,6 +56,7 @@ public class TSD_BaseClassDrivers {
 		public String strSuiteName ;
 		public String strTestNameXml;
 		public String currentBrowser;
+		public boolean blnRunRemote = false;
 //
 	  @BeforeSuite(alwaysRun=true)
 	    synchronized public void SetupEnv(ITestContext ctx ) {
@@ -67,7 +72,7 @@ public class TSD_BaseClassDrivers {
 	    @Parameters({"browser","remotePort"})    
 	    @BeforeTest(alwaysRun=true)
 	     synchronized protected void SetupTestSuite(@Optional("") String strBrowser ,
-	    						                    @Optional("4444") String strRemotePort ) {
+	    						                    @Optional("") String strRemotePort ) {
 	    	String envBrowser    = System.getenv("browserType");
 	    	String envPortRemote = System.getenv("portRemote");
 	    	if (envBrowser != null) {
@@ -77,6 +82,9 @@ public class TSD_BaseClassDrivers {
 	    	}
 	    	if (envPortRemote != null) {
 	    		strRemotePort = envPortRemote;
+	    	}
+	    	if(!strRemotePort.isEmpty()) {
+	    		blnRunRemote = true;
 	    	}
 	    	System.out.println("Current browser = " + currentBrowser);
 	    }
@@ -110,10 +118,62 @@ public class TSD_BaseClassDrivers {
 				return null;
 			}
 	    }
+
+	    DesiredCapabilities capability = new DesiredCapabilities();
+	    private void  setRemoteCapabilities(String _browser) {
+	       	OsCheck.OSType ostype=OsCheck.getOperatingSystemType();
+	       	Platform platform=null;
+	       	switch (ostype) {
+	       	    case Windows: {
+	       	    	platform= Platform.WINDOWS;
+	       	    	break;
+	       	    	}
+	       	    case MacOS:{
+	       	    	platform= Platform.MAC;
+	       	    	break;
+	       	    	}
+	       	    case Linux: {
+	       	    	platform= Platform.LINUX;
+	       	    	break;
+	       	    	}
+	       	}
+
+	    	capability.setPlatform(platform);
+	    	
+	    	try {
+		    	switch (_browser.toLowerCase()) {
+				case "chrome":{
+			    	capability.setBrowserName("chrome");
+			    	break;
+				}
+				case "firefox":{
+			    	capability.setBrowserName("firefox");
+			    	break;
+				}
+				case "edge":{
+			    	capability.setBrowserName("edge");
+			    	break;
+				}
+				
+				default:
+			    	capability.setBrowserName("chrome");
+			    	break;
+				}
+	    	}catch(Exception ex) {
+	    		System.out.println(ex.getMessage());
+	    	}
+	    }
+
 	  @BeforeClass(alwaysRun = true)
 	  public void setUpClass() throws Exception {
 		  	try {
-		    	driver = setDriverBrowser(currentBrowser);
+		  		if (blnRunRemote) {
+		  			setRemoteCapabilities(currentBrowser);
+					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);		  			
+		  		}else {
+		  			driver = setDriverBrowser(currentBrowser);
+		  		}
+
 				js = (JavascriptExecutor) driver;
 				vars = new HashMap<String, Object>();
 				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
